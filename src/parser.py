e30168ae589b761166bdd3429db72f914ce1bf55
Chad Jemmett <chad.jemmett@gmail.com>
2019-02-13 16:31:46 -0700

    Still trying to structure things

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,8 @@
-
+from data import room
 class Parser:
+    def __init__(self, rm, location):
+        self.rm = rm
+        self.location = location 
 
     def error_response(self):
         print("Sorry, I don't recognize that. \n Try N, S, E, or W. \n Or if you want to quit type Q/q")
@@ -7,10 +10,9 @@ class Parser:
 
     def get_text(self, text):
         if text == 'n' or text == 'N':
-            return "You go north"
-
+            self.location = self.rm['foyer']
         elif text == 's' or text == 'S':
-            return "s"
+            return "south"
         elif text == 'w' or text == 'W':
             return "w"
         elif text == 'e' or text == 'E':
@@ -23,3 +25,9 @@ class Parser:
             self.error_response()
 
 
+
+p = Parser(room, room['outside'])
+print(p.rm)
+print(p.location)
+print(p.get_text('n'))
+print(p.location)
