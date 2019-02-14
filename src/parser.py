731556242fde3a414315a49cc44cd8fedbcfea8d
Chad Jemmett <chad.jemmett@gmail.com>
2019-02-14 10:58:08 -0700

    Can visit all the rooms

--- src/parser.py
+++ src/parser.py
@@ -23,14 +23,27 @@ class Parser:
                 print("You can't go that way")
 
         elif text == 's' or text == 'S':
+            if self.check_room(self.player.room, 's_to'):
+                self.player.room.s_to
+                self.player.room = self.player.room.s_to
+                print(f"You are in {self.player.room.location}, {self.player.room.desc}")
+            else:
+                print("You can't go that way")
 
-            self.player.room.s_to
-            self.player.room = self.player.room.s_to
-            print(f"You are in {self.player.room.location}, It {self.player.room.desc}")
         elif text == 'w' or text == 'W':
-            print(f"You are in {self.player.room.location}, It {self.player.room.desc}")
+            if self.check_room(self.player.room, 'w_to'):
+                self.player.room.w_to
+                self.player.room = self.player.room.w_to
+                print(f"You are in {self.player.room.location}, {self.player.room.desc}")
+            else:
+                print("You can't go that way")
         elif text == 'e' or text == 'E':
-            print(f"You are in {self.player.room.location}, It {self.player.room.desc}")
+            if self.check_room(self.player.room, 'e_to'):
+                self.player.room.e_to
+                self.player.room = self.player.room.e_to
+                print(f"You are in {self.player.room.location}, {self.player.room.desc}")
+            else:
+                print("You can't go that way")
         elif text == 'l' or text == 'L':
             print(f"You are in {self.player.room.location}, It {self.player.room.desc}")
         elif text == 'q' or text == 'Q':
